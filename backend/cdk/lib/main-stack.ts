b374a92e35fe046cd14dbd397d32fe47fcd1bfc9
Edgar Perez <edgarp.dev@gmail.com>
2024-06-17 08:56:40 -0600

    [UPDATE] Added new stacks to main stack

--- backend/cdk/lib/main-stack.ts
+++ backend/cdk/lib/main-stack.ts
@@ -1,186 +1,33 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import * as cognito from "aws-cdk-lib/aws-cognito";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import * as sqs from "aws-cdk-lib/aws-sqs";
 import * as sfn from "aws-cdk-lib/aws-stepfunctions";
 import * as sfnTasks from "aws-cdk-lib/aws-stepfunctions-tasks";
 import path from "path";
 import { SqsEventSource } from "aws-cdk-lib/aws-lambda-event-sources";
+import { UsersPoolStack } from "./users-pool-stack";
+import { OrdersStack } from "./orders-stack";
+import PaymentStack from "./payments-stack";
 
 export class MainStack extends cdk.Stack {
+	private readonly env: string;
+
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
 
-		const env = this.node.tryGetContext("env");
+		this.env = this.node.tryGetContext("env");
 
-		const userPool = new cognito.UserPool(this, "CloudRestaurantUserPool", {
-			userPoolName: `cloud-restaurant-user-pool-${env}`,
-			signInAliases: {
-				email: true,
-			},
-			autoVerify: {
-				email: true,
-			},
-			selfSignUpEnabled: true,
-			passwordPolicy: {
-				minLength: 8,
-				requireLowercase: true,
-				requireUppercase: true,
-				requireDigits: true,
-				requireSymbols: true,
-			},
-		});
+		const userPoolStack = new UsersPoolStack(scope, id, props);
+		const { userPool } = userPoolStack.boostrap();
 
-		const userPoolClient = new cognito.UserPoolClient(
-			this,
-			"CloudRestaurantUserPoolClient",
-			{
-				userPool,
-				generateSecret: false,
-				authFlows: {
-					userPassword: true,
-					userSrp: true,
-				},
-			}
-		);
+		const ordersStack = new OrdersStack(scope, id, props);
+		const { ordersRestApi, ordersTable, ordersQueue } = ordersStack.boostrap(userPool);
 
-		const userPoolDomain = new cognito.UserPoolDomain(this, "UserPoolDomain", {
-			userPool,
-			cognitoDomain: {
-				domainPrefix: `cloud-restaurant-user-pool-${env}`,
-			},
-		});
-
-		const identityPool = new cognito.CfnIdentityPool(
-			this,
-			"CloudRestaurantIdentityPool",
-			{
-				identityPoolName: `cloud-restaurant-identity-pool-${env}`,
-				allowUnauthenticatedIdentities: false,
-				cognitoIdentityProviders: [
-					{
-						clientId: userPoolClient.userPoolClientId,
-						providerName: userPool.userPoolProviderName,
-					},
-				],
-			}
-		);
-
-		const authenticatedRole = new iam.Role(
-			this,
-			"CognitoDefaultAuthenticatedRole",
-			{
-				assumedBy: new iam.FederatedPrincipal(
-					"cognito-identity.amazonaws.com",
-					{
-						StringEquals: {
-							"cognito-identity.amazonaws.com:aud": identityPool.ref,
-						},
-						"ForAnyValue:StringLike": {
-							"cognito-identity.amazonaws.com:amr": "authenticated",
-						},
-					},
-					"sts:AssumeRoleWithWebIdentity"
-				),
-			}
-		);
-		// authenticatedRole.addToPolicy(
-		// 	new iam.PolicyStatement({
-		// 		actions: ["s3:ListBucket"],
-		// 		resources: ["arn:aws:s3:::your-bucket-name"],
-		// 	})
-		// );
-		new cognito.CfnIdentityPoolRoleAttachment(
-			this,
-			"IdentityPoolRoleAttachment",
-			{
-				identityPoolId: identityPool.ref,
-				roles: {
-					authenticated: authenticatedRole.roleArn,
-				},
-			}
-		);
-
-		const api = new apigateway.RestApi(this, "CloudRestaurantRestApi", {
-			restApiName: `cloud-restaurant-rest-api-${env}`,
-			defaultCorsPreflightOptions: {
-				allowOrigins: apigateway.Cors.ALL_ORIGINS,
-				allowMethods: apigateway.Cors.ALL_METHODS,
-				allowHeaders: ["Authorization", "Content-Type"],
-			},
-			deployOptions: {
-				stageName: env,
-			},
-		});
-
-		const authorizer = new apigateway.CognitoUserPoolsAuthorizer(
-			this,
-			"CognitoAuthorizer",
-			{
-				cognitoUserPools: [userPool],
-			}
-		);
-
-		const ordersQueue = new sqs.Queue(this, "CloudRestaurantOrdersQueue", {
-			queueName: `cloud-restaurant-orders-queue-${env}`,
-		});
-
-		const menuTable = new dynamodb.Table(this, "CloudRestaurantMenu", {
-			tableName: `cloud-restaurant-menu-db-${env}`,
-			partitionKey: { name: "Id", type: dynamodb.AttributeType.STRING },
-			billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-		});
-
-		const ordersTable = new dynamodb.Table(this, "CloudRestaurantOrdersTable", {
-			tableName: `cloud-restaurant-orders-${env}`,
-			partitionKey: { name: "orderId", type: dynamodb.AttributeType.STRING },
-			billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-		});
-
-		const paymentsTable = new dynamodb.Table(
-			this,
-			"CloudRestaurantPaymentTable",
-			{
-				tableName: `cloud-restaurant-payments-db-${env}`,
-				partitionKey: {
-					name: "paymentId",
-					type: dynamodb.AttributeType.STRING,
-				},
-				billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-			}
-		);
-
-		const paymentProcessorLambda = new lambda.Function(
-			this,
-			"PaymentProcessorLambda",
-			{
-				functionName: `cloud-restaurant-payment-processor-${env}`,
-				runtime: lambda.Runtime.NODEJS_20_X,
-				handler: "index.handler",
-				code: lambda.Code.fromAsset(
-					path.join(__dirname, "..", "..", "payment-processor-lambda"),
-					{
-						bundling: {
-							image: lambda.Runtime.NODEJS_20_X.bundlingImage,
-							command: [
-								"bash",
-								"-c",
-								"npm install && npm run build && cp -r dist/* /asset-output/ && cp -r node_modules /asset-output/",
-							],
-						},
-					}
-				),
-				environment: {
-					PAYMENTS_TABLE: paymentsTable.tableName,
-				},
-			}
-		);
-
-		paymentsTable.grantReadWriteData(paymentProcessorLambda);
+		const paymentStack = new PaymentStack(scope, id, props);
+		const { paymentProcessorLambda } = paymentStack.boostrap();
 
 		const processOrderStep = new sfnTasks.DynamoPutItem(
 			this,
@@ -253,7 +100,7 @@ export class MainStack extends cdk.Stack {
 			this,
 			"CloudRestaurantOrderPreparationTable",
 			{
-				tableName: `cloud-restaurant-order-preparation-${env}`,
+				tableName: `cloud-restaurant-order-preparation-${this.env}`,
 				partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
 				billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
 			}
@@ -263,14 +110,14 @@ export class MainStack extends cdk.Stack {
 			this,
 			"CloudRestaurantOrderPreparationRestApi",
 			{
-				restApiName: `cloud-restaurant-order-preparation-api-${env}`,
+				restApiName: `cloud-restaurant-order-preparation-api-${this.env}`,
 				defaultCorsPreflightOptions: {
 					allowOrigins: apigateway.Cors.ALL_ORIGINS,
 					allowMethods: apigateway.Cors.ALL_METHODS,
 					allowHeaders: ["Authorization", "Content-Type"],
 				},
 				deployOptions: {
-					stageName: env,
+					stageName: this.env,
 				},
 			}
 		);
@@ -306,7 +153,7 @@ export class MainStack extends cdk.Stack {
 			this,
 			"CloudRestaurantOrderPreparationApiLambda",
 			{
-				functionName: `cloud-restaurant-order-preparation-api-${env}`,
+				functionName: `cloud-restaurant-order-preparation-api-${this.env}`,
 				runtime: lambda.Runtime.NODEJS_20_X,
 				handler: "index.handler",
 				code: lambda.Code.fromAsset(
@@ -398,7 +245,7 @@ export class MainStack extends cdk.Stack {
 			this,
 			"TriggerStepFunctionLambda",
 			{
-				functionName: `cloud-restaurant-step-function-trigger-${env}`,
+				functionName: `cloud-restaurant-step-function-trigger-${this.env}`,
 				runtime: lambda.Runtime.NODEJS_20_X,
 				handler: "src/index.handler",
 				code: lambda.Code.fromAsset(
@@ -430,73 +277,14 @@ export class MainStack extends cdk.Stack {
 
 		ordersQueue.grantConsumeMessages(triggerStepFunctionLambda);
 
-		const apiLambda = new lambda.Function(this, "CloudRestaurantApiLambda", {
-			functionName: `cloud-restaurant-api-${env}`,
-			runtime: lambda.Runtime.NODEJS_20_X,
-			handler: "src/index.handler",
-			code: lambda.Code.fromAsset(path.join(__dirname, "..", "..", "api"), {
-				bundling: {
-					image: lambda.Runtime.NODEJS_20_X.bundlingImage,
-					command: [
-						"bash",
-						"-c",
-						"npm install && npm run build && cp -r dist/* /asset-output/ && cp -r node_modules /asset-output/",
-					],
-				},
-			}),
-			environment: {
-				MENU_TABLE: menuTable.tableName,
-				ORDERS_QUEUE_URL: ordersQueue.queueUrl,
-			},
-		});
-
-		menuTable.grantReadData(apiLambda);
-		ordersQueue.grantSendMessages(apiLambda);
-
-		const helloResource = api.root.addResource("hello");
-		helloResource.addMethod(
-			"GET",
-			new apigateway.LambdaIntegration(apiLambda),
-			{
-				authorizer,
-				authorizationType: apigateway.AuthorizationType.COGNITO,
-			}
-		);
-
-		const menuResource = api.root.addResource("menu");
-		menuResource.addMethod("GET", new apigateway.LambdaIntegration(apiLambda), {
-			authorizer,
-			authorizationType: apigateway.AuthorizationType.COGNITO,
-		});
-
-		const ordersResource = api.root.addResource("orders");
-
-		ordersResource.addMethod(
-			"POST",
-			new apigateway.LambdaIntegration(apiLambda),
-			{
-				authorizer,
-				authorizationType: apigateway.AuthorizationType.COGNITO,
-			}
-		);
-
 		// OUTPUTS
-		new cdk.CfnOutput(this, "CognitoDomain", {
-			value: `https://${userPoolDomain.domainName}.auth.${
-				cdk.Stack.of(this).region
-			}.amazoncognito.com`,
-		});
-		new cdk.CfnOutput(this, "UserPoolId", {
-			value: userPool.userPoolId,
-		});
-		new cdk.CfnOutput(this, "IdentityPoolId", {
-			value: identityPool.ref,
-		});
-		new cdk.CfnOutput(this, "ApiUrl", {
-			value: api.url,
+		new cdk.CfnOutput(this, "OrdersRestApiUrl", {
+			value: ordersRestApi.url,
 		});
 		new cdk.CfnOutput(this, "OrderPreparationApi", {
 			value: orderPreparationApi.url,
 		});
 	}
+
+	private createUserPool(): void {}
 }
