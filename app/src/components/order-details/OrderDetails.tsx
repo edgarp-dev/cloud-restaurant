d39b1b8558e15156dc8985e4cfacf3950629796b
Edgar Perez <edgarp.dev@gmail.com>
2024-07-08 08:36:45 -0600

    [UPDATE] Added basic order details layout

--- app/src/components/order-details/OrderDetails.tsx
+++ app/src/components/order-details/OrderDetails.tsx
@@ -1,15 +1,55 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { Card, Row, Col, Image } from "antd";
 
 const OrderDetails = () => {
 	const { orderId } = useParams();
 
 	return (
-		<div>
-			<h1>Order Details</h1>
-			<p>Order ID: {orderId}</p>
-			{/* Render the rest of the order details here */}
-		</div>
+		<Card
+			style={{
+				borderRadius: "10px",
+			}}
+		>
+			<Row>
+				<Col span={6}>
+					<div
+						style={{
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "center",
+							height: "100%",
+						}}
+					>
+						<Image
+							src="https://via.placeholder.com/150"
+							alt="Order Image"
+							style={{
+								width: "100px",
+								height: "200px",
+								borderRadius: "50%",
+								objectFit: "cover",
+							}}
+							preview={false}
+						/>
+					</div>
+				</Col>
+				<Col span={18}>
+					<div
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "center",
+							height: "100%",
+						}}
+					>
+						<p>Order: Pepperoni Pizza</p>
+						<p>Date: Sunday June 30 10:34 AM</p>
+						<p>Status: Order received</p>
+					</div>
+				</Col>
+			</Row>
+		</Card>
 	);
 };
 
